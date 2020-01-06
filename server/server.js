1f880a631923861373519ac06316b3995538a7e8
Dane Smith <doctorhowser@gmail.com>
2020-01-06 13:07:05 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
