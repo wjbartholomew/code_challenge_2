092ba5b8ea3fd165f2425d26f824bf142ecbde36
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-05-26 11:14:32 -0500

    jokes appended to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,20 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+    displayJokes();
 }
+
+function displayJokes(){
+console.log('hello')
+
+    $.ajax({
+        method: 'GET',
+        url: '/jokes'
+    }).then( function (response){
+        console.log(response)
+        for (i=0; i<response.length;i++){
+            $('#outputDiv').append(`${response[i].whoseJoke}<br>${response[i].jokeQuestion}<br>${response[i].punchLine} <br><br>`)
+        }
+    })
+}
+
